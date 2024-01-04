15ab7243c5e45cf14856e6a95543eca8eeb287a0
David Bernabé <david.bern.pal@gmail.com>
2024-01-04 16:34:54 +0100

    Add game controller

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+    // Make the class to Singleton
+    public static Game obj;
+
+    // Attributes about the game manager
+    public int maxLives = 3;
+    public bool isGamePaused;
+    public int score = 0;
+
+    // Call first that Start and necesary to implement the Singleton
+    void Awake() {
+        obj = this;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Define the game is not paused
+        isGamePaused = false;
+    }
+
+    // Method to save the score that player haves while is playing
+    public void AddScore(int score) {
+        this.score += score;
+    }
+
+    // Method when the game is over
+    public void GameOver() {
+        // Reset the current scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    // Call to destroy the singleton object and when the game ends
+    void OnDestroy() {
+        obj = null;
+    }
+}
