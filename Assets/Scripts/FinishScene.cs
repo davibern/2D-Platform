bc981e1703cd2175a2e44bb6dde2f8d1647128cc
David Bernabé <david.bern.pal@gmail.com>
2024-01-08 15:50:13 +0100

    Change scene when player reach the end

--- Assets/Scripts/FinishScene.cs
+++ Assets/Scripts/FinishScene.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishScene : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Player")) {
-            Debug.Log("Final!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
